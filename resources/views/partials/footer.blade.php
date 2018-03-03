aa0d89d88ae52619bb632ec4db7a611419e88a46
Gary Bunofsky <gary@garybunofsky.com>
2018-03-03 12:53:41 -0500

    Added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,77 @@
 <footer class="content-info">
   <div class="container">
     @php(dynamic_sidebar('sidebar-footer'))
+    <div class="">
+      <h5>About Us</h5>
+      <ul>
+        <li>
+          <a href="#">Item</a>
+        </li>
+        <li>
+          <a href="#">Item</a>
+        </li>
+        <li>
+          <a href="#">Item</a>
+        </li>
+        <li>
+          <a href="#">Item</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="">
+      <h5>Blog</h5>
+      <ul>
+        <li>
+          <a href="#">Item</a>
+        </li>
+        <li>
+          <a href="#">Item</a>
+        </li>
+        <li>
+          <a href="#">Item</a>
+        </li>
+        <li>
+          <a href="#">Item</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="">
+      <h5>Contact Us</h5>
+      <ul>
+        <li>
+          <a href="#">Item</a>
+        </li>
+        <li>
+          <a href="#">Item</a>
+        </li>
+        <li>
+          <a href="#">Item</a>
+        </li>
+        <li>
+          <a href="#">Item</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="">
+      <h5>Stay in Touch</h5>
+      <ul>
+        <li>
+          <a href="#">Item</a>
+        </li>
+        <li>
+          <a href="#">Item</a>
+        </li>
+        <li>
+          <a href="#">Item</a>
+        </li>
+        <li>
+          <a href="#">Item</a>
+        </li>
+      </ul>
+    </div>
+
   </div>
 </footer>
