bf45df28d9879583a0c0613e26318e072379a72a
Gary Bunofsky <gary@garybunofsky.com>
2018-03-08 17:32:21 -0500

    Added some files that style the home page

--- resources/views/template-homepage.blade.php
+++ resources/views/template-homepage.blade.php
@@ -3,8 +3,9 @@
 --}}
 
 @extends('layouts.app')
-
 @section('content')
+  @include('partials.features-section')
+  @include('partials.callout-section')
   @while(have_posts()) @php(the_post())
     @include('partials.page-header')
     @include('partials.content-page')
