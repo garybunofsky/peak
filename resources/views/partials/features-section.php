e9ca81887f0516bdde5ad24c7b0684ef3f14c17d
Gary Bunofsky <gary@garybunofsky.com>
2018-03-08 17:31:43 -0500

    Added features section

---
+++ resources/views/partials/features-section.php
@@ -0,0 +1,17 @@
+<div class="grid grid--gutters features-section">
+  <div class="grid--item">
+    <img src="/wp-content/uploads/2018/03/ruler.svg" alt="">
+    <h3>Duis Aute Irure</h3>
+    <p>Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+  </div>
+  <div class="grid--item">
+    <img src="/wp-content/uploads/2018/03/star.svg" alt="">
+    <h3>Fugiat Nulia Pariatur</h3>
+    <p>Excepteur sint occaecat cupidatat non proident. Duis aute irure dolor in reprehenderit in voluptate.</p>
+  </div>
+  <div class="grid--item">
+    <img src="/wp-content/uploads/2018/03/tag.svg" alt="">
+    <h3>Totam Rem Aperiam</h3>
+    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium, eaque ipsa quae ab illo.</p>
+  </div>
+</div>
