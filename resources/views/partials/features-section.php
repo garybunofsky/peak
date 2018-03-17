89aae8ffa9f088481b98151de1f027cfc991623d
Gary Bunofsky <gary@garybunofsky.com>
2018-03-17 13:25:04 -0400

    Added font mixin

--- resources/views/partials/features-section.php
+++ resources/views/partials/features-section.php
@@ -1,17 +1,17 @@
 <div class="grid container grid--gutters features-section">
   <div class="grid--item">
     <img src="/wp-content/uploads/2018/03/ruler.svg" alt="">
-    <h3>Duis Aute Irure</h3>
+    <h4>Duis Aute Irure</h4>
     <p>Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
   </div>
   <div class="grid--item">
     <img src="/wp-content/uploads/2018/03/star.svg" alt="">
-    <h3>Fugiat Nulia Pariatur</h3>
+    <h4>Fugiat Nulia Pariatur</h4>
     <p>Excepteur sint occaecat cupidatat non proident. Duis aute irure dolor in reprehenderit in voluptate.</p>
   </div>
   <div class="grid--item">
     <img src="/wp-content/uploads/2018/03/tag.svg" alt="">
-    <h3>Totam Rem Aperiam</h3>
+    <h4>Totam Rem Aperiam</h4>
     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium, eaque ipsa quae ab illo.</p>
   </div>
 </div>
