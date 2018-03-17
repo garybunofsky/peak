c700d024afff2a349b2482dc7071592b4a395660
Gary Bunofsky <gary@garybunofsky.com>
2018-03-17 13:46:39 -0400

    Added photo banner section.

---
+++ resources/views/partials/photo-section.blade.php
@@ -0,0 +1,7 @@
+<div class="grid photo-section">
+  <div class="content">
+    <h2>Want a free, no-obligations quote?</h2>
+    <h4>Graeci blandit maiestatis ei sit, mei at albucius epicurei, vel corrumpit torquatos. </h4>
+    <a href="#" class="button button--white button--xl">Get a Quote</a>
+  </div>
+</div>
--- resources/views/template-homepage.blade.php
+++ resources/views/template-homepage.blade.php
@@ -6,6 +6,7 @@
 @section('content')
   @include('partials.page-header')
   @include('partials.features-section')
+  @include('partials.photo-section')
   @include('partials.callout-section')
   @while(have_posts()) @php(the_post())
     @include('partials.content-page')
