2b6f3e65511724f8f9f6a416cbca413ac0ad3fa7
Gary Bunofsky <gary@garybunofsky.com>
2018-03-22 13:53:42 -0400

    Added subpage

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,3 +1,7 @@
-<div class="page-header">
-  <h1>{!! App::title() !!}</h1>
+<?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+<div class="subpage-header" style="background-image: url('<?php echo $backgroundImg[0]; ?>'); background-repeat: no-repeat;">
+  <div class="subpage-content">
+    <h1>{!! App::title() !!}</h1>
+    <h2>Suscipit quaerendum ad, viris tacimates sea at. Et modo dictas omnesque has, eam persius labores.</h2>
+  </div>
 </div>
