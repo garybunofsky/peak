06f37cf60ab6b868a6aa8436479f3cdfa644eb89
Gary Bunofsky <gary@garybunofsky.com>
2018-03-21 21:39:55 -0400

    Added utility font classes. Styled header with dark overlay

--- resources/views/partials/features-section.php
+++ resources/views/partials/features-section.php
@@ -1,17 +1,17 @@
 <div class="grid container grid--gutters grid--collapse features-section">
   <div class="grid--item">
     <img src="/wp-content/uploads/2018/03/ruler.svg" alt="">
-    <h4><?php the_field('1st_feature_title'); ?></h4>
+    <h4 class="text--xsm"><?php the_field('1st_feature_title'); ?></h4>
     <p><?php the_field('1st_feature_body'); ?></p>
   </div>
   <div class="grid--item">
     <img src="/wp-content/uploads/2018/03/star.svg" alt="">
-    <h4><?php the_field('2nd_feature_title'); ?></h4>
+    <h4 class="text--xsm"><?php the_field('2nd_feature_title'); ?></h4>
     <p><?php the_field('2nd_feature_body'); ?></p>
   </div>
   <div class="grid--item">
     <img src="/wp-content/uploads/2018/03/tag.svg" alt="">
-    <h4><?php the_field('3rd_feature_title'); ?></h4>
+    <h4 class="text--xsm"><?php the_field('3rd_feature_title'); ?></h4>
     <p><?php the_field('3rd_feature_body'); ?></p>
   </div>
 </div>
