16a6a01878970a43cd8b3514f07fb35b55852530
Gary Bunofsky <gary@garybunofsky.com>
2018-03-22 18:38:54 -0400

    Updated photo-section image

--- resources/views/partials/homepage-header.blade.php
+++ resources/views/partials/homepage-header.blade.php
@@ -2,7 +2,7 @@
 <div class="homepage-header" style="background-image: url('<?php echo $backgroundImg[0]; ?>'); background-repeat: no-repeat;">
   <div class="header-content">
     <h1>{!! App::title() !!}</h1>
-    <h2>Suscipit quaerendum ad, viris tacimates sea at. Et modo dictas omnesque has, eam persius labores.</h2>
+    <h2><?php the_field('subheading') ?></h2>
     <div class="form-box">
       <input type="text" name="" value="" placeholder="Your Email Address">
       <button class="button button--blue form-control large" type="button" name="button">Get a Quote </button>
