c9eb685a7eea286bc12d1e746b301ceda614b2c2
Gary Bunofsky <gary@garybunofsky.com>
2018-03-26 20:30:18 -0400

    Adjusted fonts

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,4 +1,4 @@
-<div class="container page-content--subpage">
+<div class="page-content--wrapper page-content--subpage">
   @php(the_content())
   {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
 </div>
