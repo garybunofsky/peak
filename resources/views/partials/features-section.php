d4b59df0928dc39ad9e8c58e6af1bc677679a354
Gary Bunofsky <gary@garybunofsky.com>
2018-04-27 20:50:36 -0400

    Adjusted nav and feature icons

--- resources/views/partials/features-section.php
+++ resources/views/partials/features-section.php
@@ -1,7 +1,7 @@
 <div class="grid container grid--gutters grid--collapse features-section">
   <div class="grid--item icon-box">
     <div class="icon-box-cell">
-      <img src="/wp-content/uploads/2018/03/ruler.svg" alt="">
+      <img src="<?= get_template_directory_uri(); ?>/assets/images/zap.svg" alt="">
     </div>
     <div class="icon-box-cell">
       <h4 class="text--xsm"><?php the_field('1st_feature_title'); ?></h4>
@@ -10,7 +10,7 @@
   </div>
   <div class="grid--item icon-box">
     <div class="icon-box-cell">
-      <img src="/wp-content/uploads/2018/03/star.svg" alt="">
+      <img src="<?= get_template_directory_uri(); ?>/assets/images/settings.svg" alt="">
     </div>
     <div class="icon-box-cell">
       <h4 class="text--xsm"><?php the_field('2nd_feature_title'); ?></h4>
@@ -19,7 +19,7 @@
   </div>
   <div class="grid--item icon-box">
     <div class="icon-box-cell">
-      <img src="/wp-content/uploads/2018/03/tag.svg" alt="">
+      <img src="<?= get_template_directory_uri(); ?>/assets/images/trending-up.svg" alt="">
     </div>
     <div class="icon-box-cell">
       <h4 class="text--xsm"><?php the_field('3rd_feature_title'); ?></h4>
