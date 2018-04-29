38aa28c0f938d40712558542651c860bc3a9d6ce
Gary Bunofsky <gary@garybunofsky.com>
2018-04-28 21:19:23 -0400

    Adjusted icon section on homepage.

--- resources/views/partials/features-section.php
+++ resources/views/partials/features-section.php
@@ -1,29 +1,26 @@
-<div class="grid container grid--gutters grid--collapse features-section">
+<div class="grid text--center container grid--gutters grid--collapse features-section">
   <div class="grid--item icon-box">
-    <div class="icon-box-cell">
+    <div class="icon-box">
       <img src="<?= get_template_directory_uri(); ?>/assets/images/zap.svg" alt="">
-    </div>
-    <div class="icon-box-cell">
-      <h4 class="text--xsm"><?php the_field('1st_feature_title'); ?></h4>
+      <h4 class="text--sm font--400"><?php the_field('1st_feature_title'); ?></h4>
       <p><?php the_field('1st_feature_body'); ?></p>
+      <a href="dry-ice-blasting-process/" class="button button--blue font--400 button--arrow">Learn More</a>
     </div>
   </div>
   <div class="grid--item icon-box">
-    <div class="icon-box-cell">
+    <div class="icon-box">
       <img src="<?= get_template_directory_uri(); ?>/assets/images/settings.svg" alt="">
-    </div>
-    <div class="icon-box-cell">
-      <h4 class="text--xsm"><?php the_field('2nd_feature_title'); ?></h4>
+      <h4 class="text--sm font--400"><?php the_field('2nd_feature_title'); ?></h4>
       <p><?php the_field('2nd_feature_body'); ?></p>
+      <a href="industries-served-with-dry-ice-blasting/" class="button button--blue font--400 button--arrow">Learn More</a>
     </div>
   </div>
   <div class="grid--item icon-box">
-    <div class="icon-box-cell">
-      <img src="<?= get_template_directory_uri(); ?>/assets/images/trending-up.svg" alt="">
-    </div>
-    <div class="icon-box-cell">
-      <h4 class="text--xsm"><?php the_field('3rd_feature_title'); ?></h4>
+    <div class="icon-box">
+      <img src="<?= get_template_directory_uri(); ?>/assets/images/plus-square.svg" alt="">
+      <h4 class="text--sm font--400"><?php the_field('3rd_feature_title'); ?></h4>
       <p><?php the_field('3rd_feature_body'); ?></p>
+      <a href="advantages-of-dry-ice-blasting/" class="button button--blue font--400 button--arrow">Learn More</a>
     </div>
   </div>
 </div>
