d2e622fdfd8510d63f83e61b8e32736a647615d5
Gary Bunofsky <gary@garybunofsky.com>
2018-04-28 21:57:17 -0400

    Install GA

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,5 +3,12 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118369648-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-118369648-2');
+  </script>
   @php(wp_head())
 </head>
--- resources/views/partials/photo-section.blade.php
+++ resources/views/partials/photo-section.blade.php
@@ -1,7 +1,7 @@
 <div class="grid photo-section">
   <div class="content">
     <h2><?php the_field('photo_bar_title'); ?></h2>
-    <h4><?php the_field('photo_bar_body'); ?></h4>
+    <h4 class="font--400"><?php the_field('photo_bar_body'); ?></h4>
     <a href="#quote-form" class="button button--white button--xl">Get a Quote →</a>
   </div>
 </div>
