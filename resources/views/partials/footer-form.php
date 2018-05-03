3cfa8633c9178dec8b09d6de4340eb8cf85a90ca
Gary Bunofsky <gary@garybunofsky.com>
2018-05-02 22:23:27 -0400

    Made some changes to nav.

--- resources/views/partials/footer-form.php
+++ resources/views/partials/footer-form.php
@@ -6,4 +6,12 @@
     </div>
     <?php echo do_shortcode('[contact-form-7 id="24" title="Homepage Form"]'); ?>
   </div>
+  <div class="social">
+    <a href="https://www.facebook.com/pg/PeakDryIceBlasting/about/" target="_blank" rel="noopener">
+      <img src="<?= get_template_directory_uri(); ?>/assets/images/linkedin.svg" alt="linkedin">
+    </a>
+    <a href="https://www.linkedin.com/company/peak-dry-ice-blasting-llc/" target="_blank" rel="noopener">
+      <img src="<?= get_template_directory_uri(); ?>/assets/images/facebook.svg" alt="facebook">
+    </a>
+  </div>
 </footer>
