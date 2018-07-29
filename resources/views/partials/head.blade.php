e5866b2fd4c042b2e4496913df81ae2485c262c9
Gary Bunofsky <gary@garybunofsky.com>
2018-07-29 14:37:29 -0400

    Added Google verification code

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="google-site-verification" content="cs1LKy7Uk7ALXgAj1cBXLytBWBWP-mQdLFL4qolJDq4" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118369648-2"></script>
